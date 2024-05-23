6dfeec8d6b8d324a92bccb192ea30e1ac302e109
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2024-05-23 17:56:54 -0300

    first commit

---
+++ src/03-forms/pages/index.ts
@@ -0,0 +1,5 @@
+export { RegisterPage } from "./RegisterPage";
+export { FormikAbstractionPage } from "./FormikAbstractionPage";
+export { FormikBasicPage } from "./FormikBasicPage";
+export { FormikComponentsPage } from "./FormikComponentsPage";
+export { FormikYupPage } from "./FormikYupPage";
